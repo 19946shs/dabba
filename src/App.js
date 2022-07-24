7a7cf2bafb5e5fd79f0505e3b60b011be1513b8c
LAPTOP-UD5SA7D6\LYDIA <shsurya20@gmail.com>
2022-07-25 00:49:53 +0530

    drag and drop added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { FirebaseOptions, getApp, initializeApp } from "@firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "@firebase/auth";
 
@@ -31,6 +31,57 @@ function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   // s3URL = await axios.get('http://54.219.221.121:8080/s3Url')
+  const [dragActive, setDragActive] = useState(false);
+  // ref
+  const inputRef = useRef(null);
+  
+  // handle drag events
+  const handleDrag = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
+  
+  // triggers when file is dropped
+  const handleDrop = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      const files = [...e.dataTransfer.files]
+      axios.get('http://54.219.221.121:8080/s3Url')
+      .then((resp) => {
+          const s3URL = resp.data.url
+          console.log('Debug FIddy :: ', files, s3URL);
+          if(files.length) {
+            axios.put(s3URL, files, {
+              headers: {
+                "Content-Type": "multipart/form-data"
+              }
+            })
+          }
+        })
+    }
+  };
+  
+  // triggers when file is selected with click
+  const handleChange = function(e) {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      console.log('Debug FIddy :: ', e.target.files);
+          // handleFiles(e.target.files);
+    }
+  };
+  
+// triggers the input when the button is clicked
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
+
   
   useEffect(() => {
     const auth = getAuth();
@@ -41,7 +92,6 @@ function App() {
       } else {
         // No user is signed in.
         setIsLoggedIn(false)
-        console.log('Debug FIddy :: ', user);
       }
     });
   }, [])
@@ -82,18 +132,16 @@ function App() {
         {
           isLoggedIn ? (
             <>
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
+              <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+                <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
+                <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
+                  <div>
+                    <p>Drag and drop your file here or</p>
+                    <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
+                  </div> 
+                </label>
+                { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
+              </form>
               <button onClick={logout}>Logout</button>
             </>
           ) : (
